e2af6c24c097b08914259147b2b93877fdf3c010
amol-tech <taj.amol@gmail.com>
2024-08-27 22:26:21 +0530

    updated css js

--- static/js/standard.js
+++ static/js/standard.js
@@ -1,66 +1,25 @@
 // Data Table Pagination
-document.addEventListener('DOMContentLoaded', function ()
-{
-    const createResizableTable = function (table)
-    {
-        const cols = table.querySelectorAll('th');
-        const updateTableWidth = () =>
-        {
-            table.style.width = [].reduce.call(
-            cols,
-            (sum, col) => sum + parseInt(col.style.width),
-            0
-            ) + 'px';
-        };
 
-        [].forEach.call(cols, function (col)
-        {
-            // Add a resizer element to the column
-            const resizer = document.createElement('div');
-            resizer.classList.add('resizer');
-            // Set the height
-            resizer.style.height = `${table.offsetHeight}px`;
-            col.appendChild(resizer);
-            createResizableColumn(col, resizer, updateTableWidth);
-        });
-        updateTableWidth();
-    };
+$("td,th")
+     .css({
+         // required to allow resizer embedding
+         position: "relative"
+     })
 
-    const createResizableColumn = function (col, resizer, updateTableWidth)
-    {
-        let x = 0;
-        let w = parseInt(window.getComputedStyle(col).width, 10);
-        col.style.width = `${w}px`;
+     .prepend("<div class='resizer'></div>")
+     .resizable({
+         resizeHeight: false,
+         // we use the column as handle and filter
+         // by the contained .resizer element
+         handleSelector: "",
+         onDragStart: function(e, $el, opt) {
+         // only drag resizer
+         if (!$(e.target).hasClass("resizer"))
+             return false;
+         return true;
+         }
+     });
 
-        const mouseDownHandler = function (e)
-        {
-            x = e.clientX;
-            const styles = window.getComputedStyle(col);
-            w = parseInt(styles.width, 10);
-            document.addEventListener('mousemove', mouseMoveHandler);
-            document.addEventListener('mouseup', mouseUpHandler);
-            resizer.classList.add('resizing');
-        };
-
-        const mouseMoveHandler = function (e)
-        {
-            const dx = e.clientX - x;
-            col.style.width = `${w + dx}px`;
-            updateTableWidth();
-        };
-
-        const mouseUpHandler = function ()
-        {
-            resizer.classList.remove('resizing');
-            document.removeEventListener('mousemove', mouseMoveHandler);
-            document.removeEventListener('mouseup', mouseUpHandler);
-        };
-
-        resizer.addEventListener('mousedown', mouseDownHandler);
-    };
-
-    createResizableTable(document.getElementById('tbl_employee'));
-});
 
 function configure_action_bar(model_name)
 {
@@ -90,6 +49,19 @@ function configure_action_bar(model_name)
     }
 }
 
+$(".panel-left").resizable({
+            handleSelector: ".splitter-v",
+            resizeHeight: false
+     });
+$(".panel-left-menu").resizable({
+            handleSelector: ".splitter-v",
+            resizeHeight: false
+     });
+ $(".panel-top").resizable({
+        handleSelector: ".splitter-h",
+        resizeWidth: false
+ });
+
 $(document).on("click", ".show-standard-popup", function (e) {
     e.preventDefault();
     var title = $(this).data('title');
@@ -106,6 +78,14 @@ $(document).ready(function() {
   $('.treeview').mdbTreeview();
 });
 
+$(document).ready(function()
+{
+    $('.vertical-menu a').click(function(){
+    $('.vertical-menu a').removeClass('active');
+    $(this).addClass('active');
+});
+
+});
 var rows = document.getElementsByClassName("tree-node-parent");
 for(r of rows)
 {
@@ -129,7 +109,8 @@ function configSelectionEvent(elementId)
                 r.style.background = "";
             }
             // highlight current selection
-            this.style.background = "lightblue";
+            var backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--thm-light-row-sel-bg-color');
+            this.style.background = backgroundColor;
         });
     }
 }
@@ -149,10 +130,11 @@ function configSelectionEventWithContextMenu(elementId)
             var rowsNotSelected = elm_table.getElementsByClassName("row-select");
             for(r of rowsNotSelected)
             {
-                r.style.background = "";
+                 r.style.background = "";
             }
             // highlight current selection
-            this.style.background = "lightblue";
+            var backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--thm-light-row-sel-bg-color');
+            this.style.background = backgroundColor;
         });
 
         r.addEventListener("contextmenu", e => {
@@ -262,3 +244,140 @@ function findParentByClass(el,class_name)
     }
     return null;
 }
+
+function MultiselectDropdown(options){
+  var config={
+    search:true,
+    height:'15rem',
+    placeholder:'select',
+    txtSelected:'selected',
+    txtAll:'All',
+    txtRemove: 'Remove',
+    txtSearch:'search',
+    ...options
+  };
+  function newEl(tag,attrs){
+    var e=document.createElement(tag);
+    if(attrs!==undefined) Object.keys(attrs).forEach(k=>{
+      if(k==='class') { Array.isArray(attrs[k]) ? attrs[k].forEach(o=>o!==''?e.classList.add(o):0) : (attrs[k]!==''?e.classList.add(attrs[k]):0)}
+      else if(k==='style'){
+        Object.keys(attrs[k]).forEach(ks=>{
+          e.style[ks]=attrs[k][ks];
+        });
+       }
+      else if(k==='text'){attrs[k]===''?e.innerHTML='&nbsp;':e.innerText=attrs[k]}
+      else e[k]=attrs[k];
+    });
+    return e;
+  }
+
+  document.querySelectorAll("select[multiple]").forEach((el,k)=>{
+
+    var div=newEl('div',{class:'multiselect-dropdown',style:{width:config.style?.width??el.clientWidth+'px',padding:config.style?.padding??''}});
+    el.style.display='none';
+    el.parentNode.insertBefore(div,el.nextSibling);
+    var listWrap=newEl('div',{class:'multiselect-dropdown-list-wrapper'});
+    var list=newEl('div',{class:'multiselect-dropdown-list',style:{height:config.height}});
+    var search=newEl('input',{class:['multiselect-dropdown-search'].concat([config.searchInput?.class??'form-control']),style:{width:'100%',display:el.attributes['multiselect-search']?.value==='true'?'block':'none'},placeholder:config.txtSearch});
+    listWrap.appendChild(search);
+    div.appendChild(listWrap);
+    listWrap.appendChild(list);
+
+    el.loadOptions=()=>{
+      list.innerHTML='';
+
+      if(el.attributes['multiselect-select-all']?.value=='true'){
+        var op=newEl('div',{class:'multiselect-dropdown-all-selector'})
+        var ic=newEl('input',{type:'checkbox'});
+        op.appendChild(ic);
+        op.appendChild(newEl('label',{text:config.txtAll}));
+
+        op.addEventListener('click',()=>{
+          op.classList.toggle('checked');
+          op.querySelector("input").checked=!op.querySelector("input").checked;
+
+          var ch=op.querySelector("input").checked;
+          list.querySelectorAll(":scope > div:not(.multiselect-dropdown-all-selector)")
+            .forEach(i=>{if(i.style.display!=='none'){i.querySelector("input").checked=ch; i.optEl.selected=ch}});
+
+          el.dispatchEvent(new Event('change'));
+        });
+        ic.addEventListener('click',(ev)=>{
+          ic.checked=!ic.checked;
+        });
+        el.addEventListener('change', (ev)=>{
+          let itms=Array.from(list.querySelectorAll(":scope > div:not(.multiselect-dropdown-all-selector)")).filter(e=>e.style.display!=='none')
+          let existsNotSelected=itms.find(i=>!i.querySelector("input").checked);
+          if(ic.checked && existsNotSelected) ic.checked=false;
+          else if(ic.checked==false && existsNotSelected===undefined) ic.checked=true;
+        });
+
+        list.appendChild(op);
+      }
+
+      Array.from(el.options).map(o=>{
+        var op=newEl('div',{class:o.selected?'checked':'',optEl:o})
+        var ic=newEl('input',{type:'checkbox',checked:o.selected});
+        op.appendChild(ic);
+        op.appendChild(newEl('label',{text:o.text}));
+
+        op.addEventListener('click',()=>{
+          op.classList.toggle('checked');
+          op.querySelector("input").checked=!op.querySelector("input").checked;
+          op.optEl.selected=!!!op.optEl.selected;
+          el.dispatchEvent(new Event('change'));
+        });
+        ic.addEventListener('click',(ev)=>{
+          ic.checked=!ic.checked;
+        });
+        o.listitemEl=op;
+        list.appendChild(op);
+      });
+      div.listEl=listWrap;
+
+      div.refresh=()=>{
+        div.querySelectorAll('span.optext, span.placeholder').forEach(t=>div.removeChild(t));
+        var sels=Array.from(el.selectedOptions);
+        if(sels.length>(el.attributes['multiselect-max-items']?.value??5)){
+          div.appendChild(newEl('span',{class:['optext','maxselected'],text:sels.length+' '+config.txtSelected}));
+        }
+        else{
+          sels.map(x=>{
+            var c=newEl('span',{class:'optext',text:x.text, srcOption: x});
+            if((el.attributes['multiselect-hide-x']?.value !== 'true'))
+              c.appendChild(newEl('span',{class:'optdel',text:'🗙',title:config.txtRemove, onclick:(ev)=>{c.srcOption.listitemEl.dispatchEvent(new Event('click'));div.refresh();ev.stopPropagation();}}));
+
+            div.appendChild(c);
+          });
+        }
+        if(0==el.selectedOptions.length) div.appendChild(newEl('span',{class:'placeholder',text:el.attributes['placeholder']?.value??config.placeholder}));
+      };
+      div.refresh();
+    }
+    el.loadOptions();
+
+    search.addEventListener('input',()=>{
+      list.querySelectorAll(":scope div:not(.multiselect-dropdown-all-selector)").forEach(d=>{
+        var txt=d.querySelector("label").innerText.toUpperCase();
+        d.style.display=txt.includes(search.value.toUpperCase())?'block':'none';
+      });
+    });
+
+    div.addEventListener('click',()=>{
+      div.listEl.style.display='block';
+      search.focus();
+      search.select();
+    });
+
+    document.addEventListener('click', function(event) {
+      if (!div.contains(event.target)) {
+        listWrap.style.display='none';
+        div.refresh();
+      }
+    });
+  });
+}
+
+window.addEventListener('load',()=>{
+  MultiselectDropdown(window.Swara);
+});
